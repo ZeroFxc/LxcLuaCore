d93a082753a0ba729b55fd5a0f0576a7f62b27ee
OxenFxc <difierline@yeah.net>
2026-02-11 21:59:15 +0800

    回滚

--- tests/test_createx.lua
+++
@@ -1,7 +0,0 @@
-local res = thread.createx(function()
-    print("Inside thread")
-    return 7
-end)
-print("Result:", res)
-assert(res == 7, "Expected 7, got " .. tostring(res))
-print("Test passed!")
